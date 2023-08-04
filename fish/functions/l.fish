ac21ef3c7cb2b18c6a9c278e188e8db198672118
Erdal TAŞKESEN <erdal@taskesen.me>
2023-08-04 11:36:13 +0000

    Add fish function `l`

---
+++ fish/functions/l.fish
@@ -0,0 +1,5 @@
+# ---------------------- general ----------------------------------------------#
+
+function l --description "List files in long format with grouping and classification"
+    ls -l --almost-all --group --group-directories-first --classify
+end
